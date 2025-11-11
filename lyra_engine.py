d172176febbc1e480e25310d2d60043275440fc8
Atsushi Nonaka <92051396+Atsushi888@users.noreply.github.com>
2025-11-11 10:04:06 +0900

    Update import for DebugPanel component

--- lyra_engine.py
+++ lyra_engine.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 from personas.persona_floria_ja import get_persona
 from components.preflight import PreflightChecker
-# from components.debug_panel import DebugPanel
+from components.dp import DebugPanel
 from components.chat_log import ChatLog
 from components.player_input import PlayerInput
 from conversation_engine import LLMConversation
