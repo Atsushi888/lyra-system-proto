9f72d849f51548359305537198a2300447a98464
Atsushi Nonaka <92051396+Atsushi888@users.noreply.github.com>
2025-11-13 03:07:41 +0900

    Fix typo in PlayerInput import statement

--- components/__init__.py
+++ components/__init__.py
@@ -1,5 +1,5 @@
 from .preflight import PreflightChecker
 from .chat_log import ChatLog
-from .player_input import PlyerInput
+from .player_input import PlayerInput
 
 __all__ = { "PreflightChecker", "ChatLog", "PlayerInput" }
