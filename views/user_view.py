26caa1b60559f2ca4af011052900a3fa06e8b66f
Atsushi Nonaka <92051396+Atsushi888@users.noreply.github.com>
2025-11-13 17:56:14 +0900

    Restore preflight rendering and comment out toggles
    Re-enable preflight rendering and comment out toggle options.

--- views/user_view.py
+++ views/user_view.py
@@ -20,9 +20,9 @@ class UserView:
 
     def render(self) -> None:
         st.caption("公開向けの軽量設定のみを表示")
-        # self.preflight.render()
-        col1, col2 = st.columns(2)
-        with col1:
-            st.toggle("字幕を表示", key="ui_show_subtitle", value=st.session_state.get("ui_show_subtitle", True))
-        with col2:
-            st.toggle("効果音を有効にする", key="ui_sfx", value=st.session_state.get("ui_sfx", True))
+        self.preflight.render()
+        # col1, col2 = st.columns(2)
+        # with col1:
+        #     st.toggle("字幕を表示", key="ui_show_subtitle", value=st.session_state.get("ui_show_subtitle", True))
+        # with col2:
+        #     st.toggle("効果音を有効にする", key="ui_sfx", value=st.session_state.get("ui_sfx", True))
