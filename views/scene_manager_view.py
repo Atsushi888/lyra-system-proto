4011d486f63dfc99892f891bd9f678eb34ea7eac
Atsushi Nonaka <92051396+Atsushi888@users.noreply.github.com>
2025-12-02 17:36:10 +0900

    Add SceneManagerView class for UI management

---
+++ views/scene_manager_view.py
@@ -0,0 +1,25 @@
+# views/scene_manager_view.py
+from __future__ import annotations
+
+import streamlit as st
+from actors.scene.scene_manager import SceneManager
+
+
+class SceneManagerView:
+    """
+    ModeSwitcher から呼ばれるためのラッパ。
+    実際の UI ロジックは SceneManager.render() に委譲。
+    """
+
+    SESSION_KEY = "scene_manager_instance"
+
+    def __init__(self) -> None:
+        if self.SESSION_KEY not in st.session_state:
+            mgr = SceneManager(path="actors/scene/scene_bonus/scene_emotion_map.json")
+            mgr.load()
+            st.session_state[self.SESSION_KEY] = mgr
+
+        self.manager: SceneManager = st.session_state[self.SESSION_KEY]
+
+    def render(self) -> None:
+        self.manager.render()
