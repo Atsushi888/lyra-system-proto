085d2b3ddef86daae8bf057ff261848eb2ca1d70
Atsushi Nonaka <92051396+Atsushi888@users.noreply.github.com>
2025-12-10 03:18:28 +0900

    Refactor imports for better readability

--- actors/narrator/narrator_ai/__init__.py
+++ actors/narrator/narrator_ai/__init__.py
@@ -1,6 +1,9 @@
-# actors/narrator/narrator_ai/__init__.py
-from .types import ChoiceKind, NarrationLine, NarrationChoice
-from .narrator_ai import NarratorAI
+from .narrator_ai import (
+    ChoiceKind,
+    NarrationLine,
+    NarrationChoice,
+    NarratorAI,
+)
 
 __all__ = [
     "ChoiceKind",
